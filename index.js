3f33c1edfcf857bb2d5210682f105fabf4995fe1
kshitij varshney <kshitijvarshney92@gmail.com>
2022-07-29 17:52:26 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mysql = require("mysql");
+
+// create connection
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "kshitijdb",
+  password: "root@123",
+  insecureAuth: true,
+});
+// connect
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Mysql connected.......");
+});
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ status: "working" });
+});
+
+app.listen("3000", () => {
+  console.log("Server started on port 3000");
+});
