6b41ed4cd8f8d8ca87a5539f18b308915e44bc1e
kshitij varshney <kshitijvarshney92@gmail.com>
2022-08-01 21:39:19 +0530

    final commit

--- index.js
+++ index.js
@@ -100,6 +100,6 @@ app.get("/get10Artist", async (req, res) => {
   });
 });
 
-app.listen("3000", () => {
-  console.log("Server started on port 3000");
+app.listen("5000", () => {
+  console.log("Server started on port 5000");
 });
